43e7a2ac31451e81333cf6845d775138722fa903
Ryan O'Rourke <rorourk2@binghamton.edu>
2019-06-11 22:16:59 -0400

    created project

---
+++ desktop/src/com/csp2/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.csp2.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.csp2.CS;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CS(), config);
+	}
+}
