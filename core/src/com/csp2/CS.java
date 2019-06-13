81a1e9c8d21c2da77e351ecf12ca0175b8541819
Ryan O'Rourke <rorourk2@binghamton.edu>
2019-06-12 22:09:45 -0400

    Trying animation

--- core/src/com/csp2/CS.java
+++ core/src/com/csp2/CS.java
@@ -4,24 +4,33 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+
+import static com.badlogic.gdx.graphics.g2d.Animation.PlayMode.LOOP;
+
 
 public class CS extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	
+	public Animation<TextureRegion> run;
+	TextureAtlas atlas;
+	private float time=0;
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		run= new Animation<TextureRegion>(.033f, atlas.findRegions("running"), LOOP);
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(run.getKeyFrame(time,true), 0, 0);
 		batch.end();
 	}
 	
