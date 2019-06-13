150979f076aa9e01e3970a820f7c21ecd5bb590b
Ryan O'Rourke <rorourk2@binghamton.edu>
2019-06-12 23:15:41 -0400

    made run animation

--- core/src/com/csp2/CS.java
+++ core/src/com/csp2/CS.java
@@ -14,23 +14,37 @@ import static com.badlogic.gdx.graphics.g2d.Animation.PlayMode.LOOP;
 
 public class CS extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
-	public Animation<TextureRegion> run;
-	TextureAtlas atlas;
+	Texture img, the;
+	TextureRegion[] frames;
+	Animation a;
 	private float time=0;
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
-		run= new Animation<TextureRegion>(.033f, atlas.findRegions("running"), LOOP);
+		the = new Texture("running.png");
+
+		TextureRegion[][] tmp=TextureRegion.split(the,420,504);
+
+		frames= new TextureRegion[9];
+
+		int i=0;
+
+		for(int x=0; x<9; x++){
+			frames[i]=tmp[0][x];
+			i++;
+		}
+
+		a=new Animation(1f/9f,frames);
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(0, 0, 0, 1);
+		time+= Gdx.graphics.getDeltaTime();
+		Gdx.gl.glClearColor((float)1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(run.getKeyFrame(time,true), 0, 0);
+		batch.draw((TextureRegion)a.getKeyFrame(time,true), (float)0, (float)0);
 		batch.end();
 	}
 	
